fd3e12f83665f33d6fb9193d002e1714540d5bad
anejad <a.nejad@gmail.com>
2018-10-24 22:46:17 -0500

    Amir's Code

---
+++ cachematrix.R
@@ -0,0 +1,30 @@
+# this function creates a matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+# this function checks first if inverse of matrix is stored in memory
+# if yes, directly takes results from cache if not it calculates it
+cacheSolve <- function(x, ...) {
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+}
